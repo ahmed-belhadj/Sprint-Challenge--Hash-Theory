9cb693b44b2ae063e83afa216ddc61c0ba4af7dc
Ahmed Belhadj <mr.ahmedbelhadj@gmail.com>
2018-10-12 11:39:43 -0500

    Update hash-tables/ex1/ex1.py
    F
    ======================================================================
    FAIL: test_correctness (__main__.Test)
    ----------------------------------------------------------------------
    Traceback (most recent call last):
      File "test_ex1.py", line 7, in test_correctness
        self.assertEqual(get_indices_of_item_weights([4, 4], 8), (1, 0))
    AssertionError: Tuples differ: (0, 0) != (1, 0)
    
    First differing element 0:
    0
    1
    
    - (0, 0)
    ?  ^
    
    + (1, 0)
    ?  ^
    
    ----------------------------------------------------------------------
    Ran 1 test in 0.001s
    
    FAILED (failures=1)

--- hash-tables/ex1/ex1.py
+++ hash-tables/ex1/ex1.py
@@ -1,6 +1,13 @@
 def get_indices_of_item_weights(weights, limit):
-  pass 
+    hash_table = {weight: weights.index(weight) for weight in weights}
+    for weight in hash_table:
+        weight2 = limit - weight
+        if weight2 in hash_table.keys():
+            return tuple([hash_table[weight], hash_table[weight2]])
+
+    return tuple()
+
 
 if __name__ == '__main__':
-  # You can write code here to test your implementation using the Python repl
-  pass 
+    # You can write code here to test your implementation using the Python repl
+    pass
